ef2c36b35cd0527c586db79972471a89d0a804b1
ahenzler <aliciahenzler@outlook.com>
2021-03-30 08:54:58 -0600

    update cell class/test

--- lib/cell.rb
+++ lib/cell.rb
@@ -18,4 +18,30 @@ class Cell
   def place_ship(cruiser)
     @ship = cruiser
   end
+
+  def fired_upon?
+    if @ship.health < @ship.length
+      true
+    else
+      false
+    end
+  end
+
+  def fire_upon
+    if empty? == false
+      @ship.hit
+    end
+  end
+ #meeting at 12:25 mst
+  # def render
+  #   if empty? == true
+  #     "."
+  #   elsif fired_upon? == true && empty? == true
+  #     "M"
+  #   elsif fired_upon? == true && empty? == false
+  #     "H"
+  #   elsif empty? == false
+  #     "S"
+  #   end
+  # end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -42,4 +42,39 @@ RSpec.describe Cell do
     expect(cell.empty?).to eq(false)
   end
 
+  it 'has a fired upon' do
+    cruiser = Ship.new("Cruiser", 3)
+    cell = Cell.new("B4")
+    cell.place_ship(cruiser)
+
+    expect(cell.fired_upon?).to eq(false)
+  end
+
+  it 'has been fired upon' do
+    cruiser = Ship.new("Cruiser", 3)
+    cell = Cell.new("B4")
+    cell.place_ship(cruiser)
+    cell.fire_upon
+
+    expect(cell.fired_upon?).to eq(true)
+  end
+
+  it 'can render' do
+    cell_1 = Cell.new("B4")
+
+    expect(cell_1.render).to eq(".")
+
+    cell_1.fire_upon
+
+    expect(cell_1.render).to eq("M")
+  end
+
+  xit 'can render' do
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+
+    expect(cell_2.render).to eq(true)
+  end
+
 end
