9bfb77b0d742e07c0ea257175f951f1c0a2efced
ahenzler <aliciahenzler@outlook.com>
2021-03-31 23:00:44 -0600

    update board class and spec

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,47 @@
 class Board
-  attr_reader
+  attr_reader :cells
 
   def initialize
-    @cells = cells
+    @cells = {}
+    add_cells
   end
+
+  def add_cells
+    ["A", "B", "C", "D"].each do |letter|
+      ["1", "2", "3", "4"].each do |number|
+        name = letter + number
+        @cells[name] = name
+      end
+    end
+  end
+
+  def valid_coordinate?(coordinate)
+    if @cells.key?(coordinate)
+      true
+    else
+      false
+    end
+  end
+
+  def valid_placement?(ship, coordinates)
+    if ship.length == coordinates.length
+      true
+    else
+      false
+    end
+  end
+
+  # def length_valid?(ship, coordinates)
+  #   if ship.length == coordinates.length
+  #     true
+  #   else
+  #     false
+  #   end
+  # end
+
+  # def consecutive?(ship, coordinates)
+  # end
+
+  # def no_diagonal?(ship, coordinates)
+  # end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,37 +3,38 @@ require './lib/cell'
 require './lib/ship'
 
 RSpec.describe Board do
-  it 'has a board' do
+  it 'is a board' do
     board = Board.new
 
     expect(board).to be_instance_of(Board)
   end
 
-  xit 'has cells' do
+  it 'has cells' do
     board = Board.new
 
+  # expected = {"A1" => cell_1,
+  #             "A2" => cell_2,
+  #             "A3" => cell_3,
+  #             "A4" => cell_4,
+  #             "B1" => cell_5,
+  #             "B2" => cell_6,
+  #             "B3" => cell_7,
+  #             "B4" => cell_8,
+  #             "C1" => cell_9,
+  #             "C2" => cell_10,
+  #             "C3" => cell_11,
+  #             "C4" => cell_12,
+  #             "D1" => cell_13,
+  #             "D2" => cell_14,
+  #             "D3" => cell_15,
+  #             "D4" => cell_16}
+  #   expect(board.cells).to eq(expected)
     expect(board.cells.keys.count).to eq(16)
     expect(board.cells.values.count).to eq(16)
   end
-                            # eq{"A1" => cell_1,
-                            #   "A2" => cell_2,
-                            #   "A3" => cell_3,
-                            #   "A4" => cell_4,
-                            #   "B1" => cell_5,
-                            #   "B2" => cell_6,
-                            #   "B3" => cell_7,
-                            #   "B4" => cell_8,
-                            #   "C1" => cell_9,
-                            #   "C2" => cell_10,
-                            #   "C3" => cell_11,
-                            #   "C4" => cell_12,
-                            #   "D1" => cell_13,
-                            #   "D2" => cell_14,
-                            #   "D3" => cell_15,
-                            #   "D4" => cell_16}
 
 
-  xit 'can validate coordinates' do
+  it 'has valid coordinates' do
     board = Board.new
 
     expect(board.valid_coordinate?("A1")).to eq(true)
@@ -43,23 +44,40 @@ RSpec.describe Board do
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
 
-  xit 'can take placements' do
+  it 'has coordinates with same length as ship' do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
-    #coordintates should be same length a ship
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
-    #coordinates should be consecutive
+  end
+
+  xit 'has consecutive coordinates' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
     expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
     expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
-    #coordinates cant be diagnol
+  end
+
+  xit 'has no diagonal coordinates' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
     expect(board.valid_placement?(cruiser, ["A1", "B2", "B3"])).to eq(false)
     expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
-    #coordinates should be GOOD!!
+  end
+
+  xit 'has good coordinates' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
     expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
     expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
   end
@@ -79,6 +97,4 @@ RSpec.describe Board do
 
     #expect(cell_3.ship) == cell_2.ship
   end
-
-
 end
